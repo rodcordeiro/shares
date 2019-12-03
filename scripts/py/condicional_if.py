525c54f93e3d6c6c15515ffff9cf73b5205e28ad
rodcordeiro <rodrigomendoncca@gmail.com>
2019-12-02 23:38:18 -0300

    Add python scripts

---
+++ scripts/py/condicional_if.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+x=1
+y=10
+if x == y:
+	print("X é igual a y")
+elif x > y:
+	print("X é maior que y")
+else:
+	print("Y é maior que  X")
---
+++ scripts/py/while.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#Definindo a variável
+x=1
+#Definindo o loop enquanto x for menor que 10
+while x<10:
+	#imprimindo o valor de X
+	print(x)
+	#Somando 1 a x, isso pode ser feito +=1 ou x= x+1
+	x += 1

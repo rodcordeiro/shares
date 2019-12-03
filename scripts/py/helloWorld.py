a82cf65509a29a1e0a0994af7b3a84abbb4d75c0
rodcordeiro <rodrigomendoncca@gmail.com>
2019-12-02 22:56:31 -0300

    Att

---
+++ scripts/py/helloWorld.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+#Esta linha indica que estou trabalhando com a codificação UTF-8, permitindo assim o uso de caracteres especiais e acentos.
+
+print('Hello Universe')
---
+++ scripts/py/variaveis.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#Variáveis: 
+# -Case sensitve;
+# -Utiliza o = para atribuir;
+# -Podem ser do tipo int, float, string, boolean (true or false)
+
+mensagem="Hello Universe"
+print(mensagem)
+
+inteiro = 1 #Variavel do tipo inteiro
+flutuante= 1.1 #Variavel do tipo flutuante
+texto = "Exemplo de string" #Variável do tipo String
+booleana = True #Variável do tipo boolean
+print(inteiro)
+print(flutuante)
+print(texto)
+print(booleana)
+

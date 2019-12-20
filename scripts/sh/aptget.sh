5d5f72437118b9617f90dcde2167032f15f700ec
rodcordeiro <rodrigomendoncca@gmail.com>
2019-12-20 15:38:44 -0300

    Atualização

--- scripts/sh/aptget.sh
+++ scripts/sh/aptget.sh
@@ -2,14 +2,14 @@
 
 echo "Nao esqueca de executar como root"
 
-apt-get update
-apt-get upgrade
+apt-get update -y
+apt-get upgrade -y
 
 apt-get check
-apt-get --fix-missing install
-apt-get dist-upgrade
-apt-get update
-apt-get upgrade
+apt-get --fix-missing install -y
+apt-get dist-upgrade -y
+apt-get update -y
+apt-get upgrade -y
 apt-get autoremove
 apt-get autoclean
 

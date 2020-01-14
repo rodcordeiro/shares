71087c0076ee22caf9ed33b3098356210c1902e3
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-01-14 00:53:16 -0300

    Update

--- scripts/py/write_json.py
+++ scripts/py/write_json.py
@@ -1,6 +1,18 @@
+# -*- coding: utf-8 -*-
+
 import json
-dados = [{'peso': 10, 'cor': 'verde'}, {'peso': 15, 'cor': 'azul'}]
-def gravar(dados):
-    with open('dados.json', 'w') as f:
-        f.write(json.dumps(dados))
-gravar(dados)
+
+teste = {"lembretes":{"Agua":"202.10","Claro":"330.00"},"Metas":{"Economizar":"Economizar 30 reais"}}
+def escreve(texto):
+	with open("dados.json","w") as file:
+		json.dump(texto,file)
+
+
+def ler():
+	file = open("dados.json","r")
+	dado = json.load(file)
+	print(dado['lembretes'])
+
+
+escreve(teste)
+ler()

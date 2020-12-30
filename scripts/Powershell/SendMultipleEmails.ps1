64608f9cfac88c2888d1b4f2b5a78d6d0373555a
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-12-30 13:35:43 -0300

    Add powershell scripts

---
+++ scripts/Powershell/SendMultipleEmails.ps1
@@ -0,0 +1,65 @@
+﻿$emailsToSend = @(
+    "helymatosc@hotmail.com",
+    "rodrigomendoncca@gmail.com"
+    )
+
+
+$USER = "YOUR_EMAIL"
+
+# Remetente
+$From = $USER
+
+# Destinatário
+
+# Assunto
+$Subject = "E-mail de teste"
+
+# Corpo do e-mail
+
+$body = @'
+<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+'@
+
+# Endereço do servidor SMTP
+$SMTPServer = "smtp.gmail.com" # Substitua esse endereço pelo endereço do servidor SMTP que for utilizar
+
+# Porta do servidor SMTP
+$SMTPPort = "587"
+
+# A senha do e-mail remetente
+$password = ConvertTo-SecureString –String "YOUR_PASSWORD" –AsPlainText -Force
+
+# Novamente o endereço de e-mail do remetente
+$userName = $USER
+
+# Aqui a variável credential está recebendo um novo objeto contendo a o usuário e a senha para a autenticação no SMTP
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+# Codificação de caracteres do texto
+$Encoding= New-Object System.Text.UTF8Encoding
+
+# Realizando o envio da mensagem
+
+$emailsToSend | ForEach-Object {
+$To = $_
+Send-Mailmessage -smtpServer $smtpServer -from $From -to $To -subject $Subject -body $Body -BodyAsHtml -priority High -encoding UTF8 -port $SMTPPort -UseSsl -Credential $credential -ErrorAction Stop
+}
+
+ 
+
---
+++ scripts/Powershell/UsingArguments.ps1
@@ -0,0 +1,6 @@
+﻿param ($client, $backupStatus)
+if (!$client -Or $client -eq $null) {
+    write-host "You must pass a -client argument"
+    return
+}
+write-host "The backup on $client exited with status $backupStatus"

425606bf789074f30ecd9de895654e416abc950c
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-03-13 22:16:12 -0300

    up

--- scripts/Powershell/vader.ps1
+++ scripts/Powershell/vader.ps1
@@ -9,7 +9,7 @@
 [console]::beep(440,500)
 [console]::beep(349,350)
 [console]::beep(523,150)
-[console]::beep(440,1000)
+[console]::beep(440,1500)
 
 [console]::beep(659,500)
 [console]::beep(659,500)

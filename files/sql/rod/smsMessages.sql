e271bc7dde4dee6af142912b68b61b1b98362da6
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 15:54:49 -0300

    [Bkp SQL] Tables

---
+++ files/sql/rod/smsMessages.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `smsMessages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

28fa148172af63de449653dab999baf6e695c2d9
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:21:45 -0300

    [Rod DB Backup] bk_tb_payments created (12/30/2024)

---
+++ files/sql/rod/bk_tb_payments.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `bk_tb_payments` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

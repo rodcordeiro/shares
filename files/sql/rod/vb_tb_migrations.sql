4d9faeb342589d4b19b9f508fcb848b082b55622
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:29:38 -0300

    [Rod DB Backup] vb_tb_migrations created (12/30/2024)

---
+++ files/sql/rod/vb_tb_migrations.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `vb_tb_migrations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

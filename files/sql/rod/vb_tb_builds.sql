9cd22bac5bafc4699c6ee1c0c9c9283d4cfe8e9a
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:29:57 -0300

    [Rod DB Backup] vb_tb_builds (12/30/2024)

--- files/sql/rod/vb_tb_builds.sql
+++ files/sql/rod/vb_tb_builds.sql
@@ -1,12 +1,1 @@
-CREATE TABLE `vb_tb_builds` (
-  `id` varchar(36) NOT NULL,
-  `owner` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `name` varchar(40) NOT NULL,
-  `lvl` int(11) NOT NULL,
-  `attribute` char(1) NOT NULL,
-  `max_workers` int(11) NOT NULL DEFAULT 2,
-  `combined` int(11) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"vb_tb_builds","Create Table":"CREATE TABLE `vb_tb_builds` (\n  `id` varchar(36) NOT NULL,\n  `owner` varchar(255) NOT NULL,\n  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),\n  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `name` varchar(40) NOT NULL,\n  `lvl` int(11) NOT NULL,\n  `attribute` char(1) NOT NULL,\n  `max_workers` int(11) NOT NULL DEFAULT 2,\n  `combined` int(11) NOT NULL DEFAULT 1,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

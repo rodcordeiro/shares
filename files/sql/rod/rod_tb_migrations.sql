26e905ce95b286e99c86c5db50200c25fc4976ca
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 15:54:54 -0300

    [Bkp SQL] Tables

---
+++ files/sql/rod/rod_tb_migrations.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `rod_tb_migrations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

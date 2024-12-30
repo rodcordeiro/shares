19abe0913cb5a5a88aea49d1b2d85e80ae0c6ac9
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:30:13 -0300

    [Rod DB Backup] MONETO_TB_LOGS (12/30/2024)

--- files/sql/rod/MONETO_TB_LOGS.sql
+++ files/sql/rod/MONETO_TB_LOGS.sql
@@ -1,7 +1,1 @@
-CREATE TABLE `MONETO_TB_LOGS` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(36) NOT NULL,
-  `log` text NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"MONETO_TB_LOGS","Create Table":"CREATE TABLE `MONETO_TB_LOGS` (\n  `id` int(11) NOT NULL AUTO_INCREMENT,\n  `uuid` varchar(36) NOT NULL,\n  `log` text NOT NULL,\n  `created_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

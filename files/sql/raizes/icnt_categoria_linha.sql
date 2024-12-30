76e8fa8209ed5f8ace7ca24f20a8b98b6b7eab4c
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:33:57 -0300

    [Rod DB Backup] icnt_categoria_linha created (12/30/2024)

---
+++ files/sql/raizes/icnt_categoria_linha.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `icnt_categoria_linha` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci

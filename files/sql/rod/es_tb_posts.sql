39316b6c8a197c1d41d99f4e826eb39460fffa6e
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:30:24 -0300

    [Rod DB Backup] es_tb_posts (12/30/2024)

--- files/sql/rod/es_tb_posts.sql
+++ files/sql/rod/es_tb_posts.sql
@@ -1,11 +1,1 @@
-CREATE TABLE `es_tb_posts` (
-  `id` varchar(36) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `text` text NOT NULL,
-  `owner` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `FK_POST_OWNER` (`owner`),
-  CONSTRAINT `FK_POST_OWNER` FOREIGN KEY (`owner`) REFERENCES `es_tb_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"es_tb_posts","Create Table":"CREATE TABLE `es_tb_posts` (\n  `id` varchar(36) NOT NULL,\n  `title` varchar(255) NOT NULL,\n  `text` text NOT NULL,\n  `owner` varchar(255) NOT NULL,\n  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),\n  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  PRIMARY KEY (`id`),\n  KEY `FK_POST_OWNER` (`owner`),\n  CONSTRAINT `FK_POST_OWNER` FOREIGN KEY (`owner`) REFERENCES `es_tb_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

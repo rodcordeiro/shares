0941170cfebfae2586cba42d4ac5376a6abdcafd
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:30:09 -0300

    [Rod DB Backup] rod_tb_user (12/30/2024)

--- files/sql/rod/rod_tb_user.sql
+++ files/sql/rod/rod_tb_user.sql
@@ -1,12 +1,1 @@
-CREATE TABLE `rod_tb_user` (
-  `id` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `refreshToken` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `device_token` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UQ_296d52f72bc42b0cfd319634a58` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"rod_tb_user","Create Table":"CREATE TABLE `rod_tb_user` (\n  `id` varchar(255) NOT NULL,\n  `password` varchar(255) NOT NULL,\n  `username` varchar(255) NOT NULL,\n  `name` varchar(255) NOT NULL,\n  `refreshToken` varchar(255) NOT NULL,\n  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),\n  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `device_token` varchar(255) DEFAULT NULL,\n  PRIMARY KEY (`id`),\n  UNIQUE KEY `UQ_296d52f72bc42b0cfd319634a58` (`username`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

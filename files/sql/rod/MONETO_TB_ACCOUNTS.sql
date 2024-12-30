fa8ee2c737d789c17388d1642a625ec3049b2f35
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:30:10 -0300

    [Rod DB Backup] MONETO_TB_ACCOUNTS (12/30/2024)

--- files/sql/rod/MONETO_TB_ACCOUNTS.sql
+++ files/sql/rod/MONETO_TB_ACCOUNTS.sql
@@ -1,18 +1,1 @@
-CREATE TABLE `MONETO_TB_ACCOUNTS` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(36) NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
-  `deleted_at` datetime(6) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `ammount` double NOT NULL,
-  `threshold` double NOT NULL,
-  `paymentType` int(11) NOT NULL,
-  `owner` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `IDX_e44cf896e3e2b8b3d51a1c4ca4` (`uuid`),
-  KEY `FK_ddd08dcd05e0095031ee665c1ab` (`paymentType`),
-  KEY `FK_6055bb1561a4304a636b042b5ea` (`owner`),
-  CONSTRAINT `FK_6055bb1561a4304a636b042b5ea` FOREIGN KEY (`owner`) REFERENCES `MONETO_TB_USERS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_ddd08dcd05e0095031ee665c1ab` FOREIGN KEY (`paymentType`) REFERENCES `MONETO_TB_PAYMENTS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"MONETO_TB_ACCOUNTS","Create Table":"CREATE TABLE `MONETO_TB_ACCOUNTS` (\n  `id` int(11) NOT NULL AUTO_INCREMENT,\n  `uuid` varchar(36) NOT NULL,\n  `created_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),\n  `updated_at` datetime(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),\n  `deleted_at` datetime(6) DEFAULT NULL,\n  `name` varchar(255) NOT NULL,\n  `ammount` double NOT NULL,\n  `threshold` double NOT NULL,\n  `paymentType` int(11) NOT NULL,\n  `owner` int(11) NOT NULL,\n  PRIMARY KEY (`id`),\n  UNIQUE KEY `IDX_e44cf896e3e2b8b3d51a1c4ca4` (`uuid`),\n  KEY `FK_ddd08dcd05e0095031ee665c1ab` (`paymentType`),\n  KEY `FK_6055bb1561a4304a636b042b5ea` (`owner`),\n  CONSTRAINT `FK_6055bb1561a4304a636b042b5ea` FOREIGN KEY (`owner`) REFERENCES `MONETO_TB_USERS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,\n  CONSTRAINT `FK_ddd08dcd05e0095031ee665c1ab` FOREIGN KEY (`paymentType`) REFERENCES `MONETO_TB_PAYMENTS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION\n) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

29941288d93c80f5eeea986f1311a92c999d7025
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:30:15 -0300

    [Rod DB Backup] bk_tb_categories (12/30/2024)

--- files/sql/rod/bk_tb_categories.sql
+++ files/sql/rod/bk_tb_categories.sql
@@ -1,17 +1,1 @@
-CREATE TABLE `bk_tb_categories` (
-  `id` varchar(36) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `category` varchar(255) DEFAULT NULL,
-  `owner` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `positive` tinyint(1) NOT NULL,
-  `internal` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `FK_category_owner` (`owner`),
-  KEY `FK_category_group` (`category`),
-  KEY `idx_category_positive` (`id`,`positive`),
-  KEY `idx_category_subcategory` (`id`,`category`),
-  CONSTRAINT `FK_category_group` FOREIGN KEY (`category`) REFERENCES `bk_tb_categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_category_owner` FOREIGN KEY (`owner`) REFERENCES `bk_tb_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"bk_tb_categories","Create Table":"CREATE TABLE `bk_tb_categories` (\n  `id` varchar(36) NOT NULL,\n  `name` varchar(255) NOT NULL,\n  `category` varchar(255) DEFAULT NULL,\n  `owner` varchar(255) NOT NULL,\n  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),\n  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `positive` tinyint(1) NOT NULL,\n  `internal` tinyint(1) NOT NULL DEFAULT 0,\n  PRIMARY KEY (`id`),\n  KEY `FK_category_owner` (`owner`),\n  KEY `FK_category_group` (`category`),\n  KEY `idx_category_positive` (`id`,`positive`),\n  KEY `idx_category_subcategory` (`id`,`category`),\n  CONSTRAINT `FK_category_group` FOREIGN KEY (`category`) REFERENCES `bk_tb_categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,\n  CONSTRAINT `FK_category_owner` FOREIGN KEY (`owner`) REFERENCES `bk_tb_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

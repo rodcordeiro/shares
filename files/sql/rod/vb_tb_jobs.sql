47e42b2602c2bbbd45345a2e50748bcb59da629b
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:30:23 -0300

    [Rod DB Backup] vb_tb_jobs (12/30/2024)

--- files/sql/rod/vb_tb_jobs.sql
+++ files/sql/rod/vb_tb_jobs.sql
@@ -1,11 +1,1 @@
-CREATE TABLE `vb_tb_jobs` (
-  `id` varchar(36) NOT NULL,
-  `owner` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `name` varchar(40) NOT NULL,
-  `place` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_build_job` (`place`),
-  CONSTRAINT `FK_build_job` FOREIGN KEY (`place`) REFERENCES `vb_tb_builds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"vb_tb_jobs","Create Table":"CREATE TABLE `vb_tb_jobs` (\n  `id` varchar(36) NOT NULL,\n  `owner` varchar(255) NOT NULL,\n  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),\n  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `name` varchar(40) NOT NULL,\n  `place` varchar(255) NOT NULL,\n  PRIMARY KEY (`id`),\n  KEY `FK_build_job` (`place`),\n  CONSTRAINT `FK_build_job` FOREIGN KEY (`place`) REFERENCES `vb_tb_builds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

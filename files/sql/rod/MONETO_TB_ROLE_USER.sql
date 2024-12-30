eb034c0997dcf552eeb3ccce970269270b6ac62f
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-30 16:30:31 -0300

    [Rod DB Backup] MONETO_TB_ROLE_USER (12/30/2024)

--- files/sql/rod/MONETO_TB_ROLE_USER.sql
+++ files/sql/rod/MONETO_TB_ROLE_USER.sql
@@ -1,10 +1,1 @@
-CREATE TABLE `MONETO_TB_ROLE_USER` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(36) NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
-  `deleted_at` datetime(6) DEFAULT NULL,
-  `role` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `IDX_adf073cf0dcf5ef0bb16a8f985` (`uuid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"MONETO_TB_ROLE_USER","Create Table":"CREATE TABLE `MONETO_TB_ROLE_USER` (\n  `id` int(11) NOT NULL AUTO_INCREMENT,\n  `uuid` varchar(36) NOT NULL,\n  `created_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),\n  `updated_at` datetime(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),\n  `deleted_at` datetime(6) DEFAULT NULL,\n  `role` varchar(255) NOT NULL,\n  PRIMARY KEY (`id`),\n  UNIQUE KEY `IDX_adf073cf0dcf5ef0bb16a8f985` (`uuid`)\n) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

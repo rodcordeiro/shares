94d6a7bd9a0562700436a15594989e3fd02b7f46
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-04-16 15:47:08 -0300

    teste

--- files/sql/raizes/tb_categorias.schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `tb_categorias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UQ_8c7de51edc8bfc9c9271f64dd98` (`nome`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

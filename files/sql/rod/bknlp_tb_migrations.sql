81dbe8cb9ce82865cf02e05f8e646f7978fe0b0b
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-10-25 01:25:36 -0300

    [Rod DB Backup] bknlp_tb_migrations created (10/25/2025)

---
+++ files/sql/rod/bknlp_tb_migrations.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `bknlp_tb_migrations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

7ee3fc1fa07f8d38908bd6ea229dc3e1c5224940
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-11-07 13:53:08 -0300

    [Rod DB Backup] kersef_tb_user (11/7/2025)

--- files/sql/rod/kersef_tb_user.sql
+++ files/sql/rod/kersef_tb_user.sql
@@ -1,15 +1,1 @@
-CREATE TABLE `kersef_tb_user` (
-  `id` varchar(36) NOT NULL,
-  `userId` varchar(255) NOT NULL,
-  `guildId` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `xp` int(11) NOT NULL DEFAULT 0,
-  `level` int(11) NOT NULL DEFAULT 1,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `power_level` int(11) DEFAULT NULL,
-  `last_update_power_level` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`userId`,`guildId`),
-  KEY `IDX_user_guild_xp` (`guildId`,`xp`),
-  KEY `IDX_user_guild_name` (`guildId`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"kersef_tb_user","Create Table":"CREATE TABLE `kersef_tb_user` (\n  `id` varchar(36) NOT NULL,\n  `userId` varchar(255) NOT NULL,\n  `guildId` varchar(255) NOT NULL,\n  `name` varchar(255) DEFAULT NULL,\n  `xp` int(11) NOT NULL DEFAULT 0,\n  `level` int(11) NOT NULL DEFAULT 1,\n  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),\n  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `power_level` int(11) DEFAULT NULL,\n  `last_update_power_level` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  PRIMARY KEY (`userId`,`guildId`),\n  KEY `IDX_user_guild_xp` (`guildId`,`xp`),\n  KEY `IDX_user_guild_name` (`guildId`,`name`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

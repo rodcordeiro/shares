bcba7b591004439c1d38d223cab04999fd7191c3
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-12-16 17:00:03 -0300

    [Rod DB Backup] devices (12/16/2025)

--- files/sql/rod/devices.sql
+++ files/sql/rod/devices.sql
@@ -1,11 +1,1 @@
-CREATE TABLE `devices` (
-  `id` char(36) NOT NULL,
-  `device_id` varchar(100) NOT NULL,
-  `plant_id` char(36) DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `plant_id` (`plant_id`),
-  CONSTRAINT `devices_ibfk_1` FOREIGN KEY (`plant_id`) REFERENCES `plants` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"devices","Create Table":"CREATE TABLE `devices` (\n  `id` char(36) NOT NULL,\n  `device_id` varchar(100) NOT NULL,\n  `plant_id` char(36) DEFAULT NULL,\n  `created_at` datetime DEFAULT current_timestamp(),\n  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `deleted_at` datetime DEFAULT NULL,\n  PRIMARY KEY (`id`),\n  KEY `plant_id` (`plant_id`),\n  CONSTRAINT `devices_ibfk_1` FOREIGN KEY (`plant_id`) REFERENCES `plants` (`id`) ON DELETE SET NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

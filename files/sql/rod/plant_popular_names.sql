88702a934b7f8683621de5d6ef9082d1b3938272
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-12-16 16:59:46 -0300

    [Rod DB Backup] plant_popular_names (12/16/2025)

--- files/sql/rod/plant_popular_names.sql
+++ files/sql/rod/plant_popular_names.sql
@@ -1,11 +1,1 @@
-CREATE TABLE `plant_popular_names` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `plant_id` char(36) NOT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `plant_id` (`plant_id`),
-  CONSTRAINT `plant_popular_names_ibfk_1` FOREIGN KEY (`plant_id`) REFERENCES `plants` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"plant_popular_names","Create Table":"CREATE TABLE `plant_popular_names` (\n  `id` int(11) NOT NULL AUTO_INCREMENT,\n  `name` varchar(100) NOT NULL,\n  `plant_id` char(36) NOT NULL,\n  `created_at` datetime DEFAULT current_timestamp(),\n  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `deleted_at` datetime DEFAULT NULL,\n  PRIMARY KEY (`id`),\n  KEY `plant_id` (`plant_id`),\n  CONSTRAINT `plant_popular_names_ibfk_1` FOREIGN KEY (`plant_id`) REFERENCES `plants` (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

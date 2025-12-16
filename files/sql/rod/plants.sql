2599ae290b8a489f27f3e57f22326d85f81cd192
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-12-16 17:00:10 -0300

    [Rod DB Backup] plants (12/16/2025)

--- files/sql/rod/plants.sql
+++ files/sql/rod/plants.sql
@@ -1,12 +1,1 @@
-CREATE TABLE `plants` (
-  `id` char(36) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `scientific_name` varchar(100) DEFAULT NULL,
-  `solar_light` enum('direta_longa_duracao','direta_curta_duracao','indireta','sombra') DEFAULT NULL,
-  `humidity_percent` decimal(5,2) DEFAULT NULL,
-  `last_fertilization` datetime DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"plants","Create Table":"CREATE TABLE `plants` (\n  `id` char(36) NOT NULL,\n  `name` varchar(100) NOT NULL,\n  `scientific_name` varchar(100) DEFAULT NULL,\n  `solar_light` enum('direta_longa_duracao','direta_curta_duracao','indireta','sombra') DEFAULT NULL,\n  `humidity_percent` decimal(5,2) DEFAULT NULL,\n  `last_fertilization` datetime DEFAULT NULL,\n  `created_at` datetime DEFAULT current_timestamp(),\n  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `deleted_at` datetime DEFAULT NULL,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

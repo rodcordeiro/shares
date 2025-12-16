7f848a69d8c5c14e97fcecfb7b6246ea62f124f4
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-12-16 16:59:32 -0300

    [Rod DB Backup] orisas created (12/16/2025)

---
+++ files/sql/rod/orisas.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `orisas` (
+  `id` char(36) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `xire_order` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

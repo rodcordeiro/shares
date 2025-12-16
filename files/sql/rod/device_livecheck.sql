c458e322ed042d99f83cccc1c6301ad672613b71
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-12-16 17:00:16 -0300

    [Rod DB Backup] device_livecheck (12/16/2025)

--- files/sql/rod/device_livecheck.sql
+++ files/sql/rod/device_livecheck.sql
@@ -1,10 +1,1 @@
-CREATE TABLE `device_livecheck` (
-  `id` char(36) NOT NULL,
-  `device_id` char(36) NOT NULL,
-  `temperature` decimal(5,2) NOT NULL,
-  `humidity` decimal(5,2) NOT NULL,
-  `timestamp` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `device_id` (`device_id`),
-  CONSTRAINT `device_livecheck_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"device_livecheck","Create Table":"CREATE TABLE `device_livecheck` (\n  `id` char(36) NOT NULL,\n  `device_id` char(36) NOT NULL,\n  `temperature` decimal(5,2) NOT NULL,\n  `humidity` decimal(5,2) NOT NULL,\n  `timestamp` datetime DEFAULT current_timestamp(),\n  PRIMARY KEY (`id`),\n  KEY `device_id` (`device_id`),\n  CONSTRAINT `device_livecheck_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

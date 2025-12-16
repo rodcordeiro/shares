a5cb4fe1df57c548251365cde16737b90e54967f
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-12-16 16:59:33 -0300

    [Rod DB Backup] religious_lines created (12/16/2025)

---
+++ files/sql/rod/religious_lines.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `religious_lines` (
+  `id` char(36) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

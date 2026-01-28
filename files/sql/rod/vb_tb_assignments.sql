ab9163a82c680632169013aaf0e4d7db4de1415d
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2026-01-28 19:43:17 -0300

    [Rod DB Backup] vb_tb_assignments (1/28/2026)

--- files/sql/rod/vb_tb_assignments.sql
+++ files/sql/rod/vb_tb_assignments.sql
@@ -1,13 +1,1 @@
-CREATE TABLE `vb_tb_assignments` (
-  `id` varchar(36) NOT NULL,
-  `owner` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `dweller` varchar(40) NOT NULL,
-  `job` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_job_assignment` (`job`),
-  KEY `FK_dweller_assignment` (`dweller`),
-  CONSTRAINT `FK_dweller_assignment` FOREIGN KEY (`dweller`) REFERENCES `vb_tb_dwellers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_job_assignment` FOREIGN KEY (`job`) REFERENCES `vb_tb_jobs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+{"Table":"vb_tb_assignments","Create Table":"CREATE TABLE `vb_tb_assignments` (\n  `id` varchar(36) NOT NULL,\n  `owner` varchar(255) NOT NULL,\n  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),\n  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\n  `dweller` varchar(40) NOT NULL,\n  `job` varchar(255) NOT NULL,\n  PRIMARY KEY (`id`),\n  KEY `FK_job_assignment` (`job`),\n  KEY `FK_dweller_assignment` (`dweller`),\n  CONSTRAINT `FK_dweller_assignment` FOREIGN KEY (`dweller`) REFERENCES `vb_tb_dwellers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,\n  CONSTRAINT `FK_job_assignment` FOREIGN KEY (`job`) REFERENCES `vb_tb_jobs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"}

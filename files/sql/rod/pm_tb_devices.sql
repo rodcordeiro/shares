c1cfc4787fa541d01985e4ce182edc29f4b06c2b
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2026-01-28 19:42:28 -0300

    [Rod DB Backup] pm_tb_devices (1/28/2026)

--- files/sql/rod/pm_tb_devices.sql
+++ files/sql/rod/pm_tb_devices.sql
@@ -1,16 +1,1 @@
-CREATE TABLE `pm_tb_devices` (
-  `id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL DEFAULT uuid(),
-  `device_id` varchar(150) NOT NULL,
-  `plant_id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `created_by` varchar(16) DEFAULT NULL,
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_by` varchar(16) DEFAULT NULL,
-  `deleted_at` datetime(6) NOT NULL,
-  `deleted_by` char(36) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL,
-  `deleted` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IX_pm_tb_devices_created_at` (`created_at`),
-  KEY `IX_pm_tb_devices_deleted` (`deleted`),
-  KEY `IX_pm_tb_devices_updated_at` (`updated_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci
+{"Table":"pm_tb_devices","Create Table":"CREATE TABLE `pm_tb_devices` (\n  `id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL DEFAULT uuid(),\n  `device_id` varchar(150) NOT NULL,\n  `plant_id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL,\n  `created_at` datetime NOT NULL DEFAULT current_timestamp(),\n  `created_by` varchar(16) DEFAULT NULL,\n  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),\n  `updated_by` varchar(16) DEFAULT NULL,\n  `deleted_at` datetime(6) NOT NULL,\n  `deleted_by` char(36) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL,\n  `deleted` tinyint(1) NOT NULL,\n  PRIMARY KEY (`id`),\n  KEY `IX_pm_tb_devices_created_at` (`created_at`),\n  KEY `IX_pm_tb_devices_deleted` (`deleted`),\n  KEY `IX_pm_tb_devices_updated_at` (`updated_at`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci"}

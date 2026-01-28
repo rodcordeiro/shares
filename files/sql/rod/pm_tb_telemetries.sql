180aee83f53a457c7d11fe02190acc72d04525ed
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2026-01-28 19:42:34 -0300

    [Rod DB Backup] pm_tb_telemetries (1/28/2026)

--- files/sql/rod/pm_tb_telemetries.sql
+++ files/sql/rod/pm_tb_telemetries.sql
@@ -1,9 +1,1 @@
-CREATE TABLE `pm_tb_telemetries` (
-  `id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL DEFAULT uuid(),
-  `device_id` varchar(255) NOT NULL,
-  `temperature` decimal(65,30) DEFAULT NULL,
-  `humidity` decimal(65,30) DEFAULT NULL,
-  `timestamp` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IX_pm_tb_telemetries_device_id` (`device_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci
+{"Table":"pm_tb_telemetries","Create Table":"CREATE TABLE `pm_tb_telemetries` (\n  `id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL DEFAULT uuid(),\n  `device_id` varchar(255) NOT NULL,\n  `temperature` decimal(65,30) DEFAULT NULL,\n  `humidity` decimal(65,30) DEFAULT NULL,\n  `timestamp` datetime(6) NOT NULL,\n  PRIMARY KEY (`id`),\n  KEY `IX_pm_tb_telemetries_device_id` (`device_id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci"}

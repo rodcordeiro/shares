4b0613e54d47ef55572d2adab9ed431bce00132c
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2026-01-28 19:42:18 -0300

    [Rod DB Backup] __EFMigrationsHistory created (1/28/2026)

---
+++ files/sql/rod/__EFMigrationsHistory.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `__EFMigrationsHistory` (
+  `MigrationId` varchar(150) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci

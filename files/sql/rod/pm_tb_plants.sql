b04697da38995f0b8f8ace91029972c6fac7f091
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2026-01-28 19:42:21 -0300

    [Rod DB Backup] pm_tb_plants created (1/28/2026)

---
+++ files/sql/rod/pm_tb_plants.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `pm_tb_plants` (
+  `id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL DEFAULT uuid(),
+  `name` varchar(150) NOT NULL,
+  `scientific_name` longtext DEFAULT NULL,
+  `solar_light` longtext NOT NULL,
+  `humidity_percent` decimal(65,30) NOT NULL,
+  `last_fertilization` datetime(6) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(16) DEFAULT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(16) DEFAULT NULL,
+  `deleted_at` datetime(6) NOT NULL,
+  `deleted_by` char(36) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IX_pm_tb_plants_created_at` (`created_at`),
+  KEY `IX_pm_tb_plants_deleted` (`deleted`),
+  KEY `IX_pm_tb_plants_id` (`id`),
+  KEY `IX_pm_tb_plants_last_fertilization` (`last_fertilization`),
+  KEY `IX_pm_tb_plants_updated_at` (`updated_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci
